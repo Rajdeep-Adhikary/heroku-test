a709cf0ae687143e509d1fff4eb8d174d6e5704b
Rajdeep Adhikary <rajdeep@redappletech.com>
2023-05-17 17:25:09 +0530

    updated cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const socketio = require('socket.io');
 const server = http.createServer(app);
 const io = socketio(server, {
     cors : {
-        origin : ['http://localhost']
+        origin : ['http://localhost', 'https://rajdeepadhikary.com']
     }
 });
 
