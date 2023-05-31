2a5f1586d9dfb59000da1545a7f15e86e138d05f
rajdeep-adhikary <rajdeepadhikary@proton.me>
2023-05-31 13:28:29 +0530

    configure websocket for structure

--- src/websockets/websocket.js
+++ src/websockets/websocket.js
@@ -1,24 +1,46 @@
-let setNSP = (gameIo) => {
-    let auth = require('../middlewares/auth');
-    console.log(`socket server listening on NameSpace : ${gameIo.name}`);
-    gameIo.use(auth.isAuthorizedSocket).on('connection',async (socket) => {
-
-        /**
-         * Connection Handler.
-        **/
-        console.log(`one socket connected:${socket.id} with user_id:${socket.user.user_id}`);
-
-        /**
-         * Socket Events For Application Logic.
-        **/
-
-        /**
-         * Disconnection Handler.
-        **/
-        socket.on('disconnect',async () => {
-            console.log(`one socket disconnected:${socket.id} with user_id:${socket.user.user_id}`);
+let setNSP = (io) => {
+    io.on('connection', (socket) => {
+        console.log('Socket Connected');
+    
+    
+        socket.on('send-message', (user, message, room) => {
+            if(room === '')
+                socket.broadcast.emit('send-to-other', user, message); 
+            else
+                socket.to(room).emit('send-to-other', user, message);
+        })
+    
+        socket.on('join-room', (room, user, callback) => {
+            if(room === '' || room === undefined){
+                callback({ msg : '', error : 'Room is required' });
+                return false;
+            }
+            if(user === '' || user === undefined){
+                callback({ msg : '', error : 'Username is required' });
+                return false;
+            }
+            if(users.exist(user, room)){
+                callback({ msg : '', error : 'Username already exist in this room' });
+                return false;
+            }
+            users.addUser(user, room);
+            socket.join(room);
+            callback({ message : `You have joined to ${room} room`, error : '' });
+            var all_users = users.getUserByRoom(room);
+            socket.to(room).emit('user-joined', user);
+            io.in(room).emit('update-user-list', all_users);
+        })
+    
+        socket.on('leave-room', (room, user, callback) => {
+            socket.leave(room);
+            callback()
+            socket.to(room).emit('user-left', user);
+        })
+    
+        socket.on('disconnect', function() {
+            clients.splice(clients.indexOf(client), 1);
         });
-    });
+    })
 }
 
 module.exports = {
--- www/socket/ws.js
+++ www/socket/ws.js
@@ -4,13 +4,13 @@ const appConfig = require('../../config/appConfig');
 
 
 let startSocket = (server) => {
-    let io = socketio(server,{
-        cors: {
-          origin: '*',
-        }
-      });
-    let gameIO = io.of(`/${appConfig.socketNameSpace}`);
-    websocket.setNSP(gameIO);
+  const io = socketio(server, {
+      cors : {
+          origin : ['http://localhost', 'https://www.rajdeepadhikary.com', 'https://rajdeepadhikary.com']
+      }
+  });
+    // let gameIO = io.of(`/${appConfig.socketNameSpace}`);
+    websocket.setNSP(io);
 }
 
 module.exports = {
